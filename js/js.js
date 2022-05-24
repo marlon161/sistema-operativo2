12e3d5bda6b7557e22c63124458cca7e047b6466
Marlon <totoreto3martinez@gmail.com>
2022-05-24 12:45:41 -0600

    agregue nuevos botones

--- js/js.js
+++ js/js.js
@@ -2,7 +2,23 @@ function abrirVentana(){
     var ventana = document.getElementById('ventana');
     ventana.classList.add("ventanaActivar");
     
-    
 
 }
+function CerrarVentana(){
+    var ventana = document.getElementById('ventana');
+    ventana.classList.remove("ventanaActivar");
+}
+
+function ExpandirVentana(){
+    var ventana = document.getElementById('ventana');
+    ventana.classList.add("Expandir");
+}
+
+function minimizarVentana(){
+    var ventana = document.getElementById('ventana');
+    ventana.classList.remove("Expandir");
+
+}
+
+
 
